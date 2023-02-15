b52b26ec56578fd8d13f6c3461af32b738b12446
tn <tn@tn>
2023-02-15 17:19:09 +0800

    引入Naive UI,配置组件自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -11,8 +7,17 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+ 手动导入
+  <div>
+  <NButton>naive-ui</NButton>
+  </div>
+
 </template>
+<script setup lang="ts">
+//手动导入
+// import { NButton } from 'naive-ui'
+
+</script>
 
 <style scoped>
 .logo {
